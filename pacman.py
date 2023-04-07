3953b3be796bb56c5095411c9cc08168954725df
Edward Ryan <er.ryan@icloud.com>
2023-04-07 18:45:21 -0400

    first

---
+++ pacman.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Environment: https://gymnasium.farama.org/environments/atari/pacman/
+
+# Learning points:
+# * Observation and Reward function is clearly defined here (simplistic relative to practical or "real life" situations)
+
+import gymnasium as gym
+
+STEPS=10**3
+
+def main():
+  env = gym.make("ALE/Pacman-v5", render_mode="human")
+
+  env.reset()
+  for _ in range(STEPS):
+    # action = env.action_space  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(0)
+    if terminated or truncated:
+      observation, info = env.reset()
+  env.close()
+
+if __name__=="__main__":
+  main()
