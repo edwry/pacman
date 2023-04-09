92358d1b4437683bf8acc84ace9bccc689d25eb9
Edward Ryan <er.ryan@icloud.com>
2023-04-09 18:02:24 -0400

    hi there

--- pacman.py
+++ pacman.py
@@ -14,8 +14,8 @@ def main():
 
   env.reset()
   for _ in range(STEPS):
-    # action = env.action_space  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(0)
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
       observation, info = env.reset()
   env.close()
